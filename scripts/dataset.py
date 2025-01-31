36881800b628192a0b2a58e86390b06ee7c56bfd
Matteo <matteo.kolyszko@polito.it>
2025-01-31 14:53:38 +0100

    updated models.py

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -16,9 +16,9 @@ class Hdf5Dataset(Dataset):
 
         # File paths
         self.hr_dataset = h5py.File(f'{self.base}/hr{val}.h5', 'r')['/data']
-        self.out_dataset = h5py.File(f'{self.base}/out{val}.h5', 'r')['/data']
+        #self.out_dataset = h5py.File(f'{self.base}/out{val}.h5', 'r')['/data']
         self.replaced_dataset = h5py.File(f'{self.base}/replaced{val}.h5', 'r')['/data']
-        self.hr_g_dataset = h5py.File(f'{self.base}/hr_g{val}.h5', 'r')['/data']
+        #self.hr_g_dataset = h5py.File(f'{self.base}/hr_g{val}.h5', 'r')['/data']
 
     def __getitem__(self, index):
         x, y = self.replaced_dataset[index], self.hr_dataset[index]
